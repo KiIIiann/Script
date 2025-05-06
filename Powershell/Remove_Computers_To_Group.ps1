ee02b0075681cd1dcd1bcddac1f57293794286b6
KiIIiann <killian.gonin@protonmail.com>
2025-05-06 13:12:24 +0200

    Update Remove_Computers_To_Group.ps1

--- Powershell/Remove_Computers_To_Group.ps1
+++ Powershell/Remove_Computers_To_Group.ps1
@@ -1,17 +1,15 @@
 
 #Script Powershell permettant de supprimer plusieurs machines d'un même groupe AD
 
-$Computers=@("TestComputer","TestComputer2")
-$GroupName="Test"
-
-
 # Vérifie la disponibilité du module ActiveDirectory
 if (-not (Get-Module -ListAvailable -Name ActiveDirectory)) {
     Write-Error "Le module ActiveDirectory n'est pas installé."
     exit 1
 }
-
 Import-Module ActiveDirectory
+
+$Computers=@("TestComputer","TestComputer2")
+$GroupName="Test"
 
 foreach ($computer in $Computers) {
     try {
