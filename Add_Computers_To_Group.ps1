4579958fe717dffde16d3e0b1416ec379fd13ac7
KiIIiann <killian.gonin@protonmail.com>
2025-05-06 13:02:53 +0200

    Create Add_Computers_To_Group.ps1

---
+++ Add_Computers_To_Group.ps1
@@ -0,0 +1,26 @@
+
+#Script Powershell permettant de supprimer plusieurs machines d'un même groupe AD
+
+$Computers=@("TestComputer","TestComputer2")
+$GroupName="Test"
+
+
+# Vérifie la disponibilité du module ActiveDirectory
+if (-not (Get-Module -ListAvailable -Name ActiveDirectory)) {
+    Write-Error "Le module ActiveDirectory n'est pas installé."
+    exit 1
+}
+
+Import-Module ActiveDirectory
+
+foreach ($computer in $Computers) {
+    try {
+        $SelectedComputer = Get-ADComputer -Identity $computer -ErrorAction Stop
+
+        Remove-ADGroupMember -Identity $GroupName -Members $SelectedComputer -Confirm:$false
+        Write-Host "[$computer] supprimé du groupe [$GroupName]" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Erreur avec [$computer] : $_" -ForegroundColor Red
+    }
+}
